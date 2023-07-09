8d12718a40006d80ed6993a1c6d9646936a453cd
Tulu <1805047@ugrad.cse.buet.ac.bd>
2023-07-10 02:38:15 +0600

    message done

--- routes/message.js
+++ routes/message.js
@@ -46,5 +46,44 @@ router.post('/send', (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /message/receive:
+ *   get:
+ *     summary: receive message
+ *     tags: [Message]
+ *     description: receive message
+ *     parameters:
+ *       - in: query
+ *         name: sender_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: sender id
+ *     responses:
+ *       200:
+ *         description: The message was successfully received
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
+
+router.get('/receive', (req, res) => {
+    let sender_id = req.query.sender_id;
+    return res.send([
+        {
+            sender_id: sender_id,
+            message_id: "message_id",
+            message: "received message 1",
+        },
+        {
+            sender_id: sender_id,
+            message_id: "message_id",
+            message: "received message 2",
+        }
+    ]);
+});
+
 
 module.exports = router;
