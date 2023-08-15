6ef01d21cd65fb965f8a5d4ed6079cd0707fc4fe
Tulu <1805047@ugrad.cse.buet.ac.bd>
2023-08-15 10:14:20 +0600

    fixed-values sending with ids'

--- controller/fixed-values.js
+++ controller/fixed-values.js
@@ -5,14 +5,14 @@ class FixedValuesController {
   getFacilities = async (req, res) => {
     console.log("FixedValuesController::getFacilities");
     let data = await repo.getFacilities();
-    data = data.map(datum => datum.title);
+    // data = data.map(datum => datum.title);
     res.status(200).json(data);
   }
 
   getStarPoints = async (req, res) => {
     console.log("FixedValuesController::getStarPoints");
     let data = await repo.getStarPoints();
-    data = data.map(datum => datum.title);
+    // data = data.map(datum => datum.title);
     res.status(200).json(data);
   }
 }
