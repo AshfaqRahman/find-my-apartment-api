9ebc99bd442fc64a2c0a6df9c1f04f79305f1d9f
AshfaqRahman <1805047@ugrad.cse.buet.ac.bd>
2023-08-20 15:15:45 +0600

    getPreference added

--- controller/user.js
+++ controller/user.js
@@ -4,9 +4,19 @@ const repo = new UserRepository();
 class UserController {
 
 
-  preference = async (req, res) => {
-    console.log("UserController::preference");
-    const { data, error } = await repo.preference(req.body);
+  setPreference = async (req, res) => {
+    console.log("UserController::setPreference");
+    const { data, error } = await repo.setPreference(req.body);
+    if (error) {
+      res.status(500).json(error);
+    }
+    res.status(200).json(data);
+  };
+
+
+  getPreference = async (req, res) => {
+    console.log("UserController::getPreference");
+    const { data, error } = await repo.getPreference(req.body);
     if (error) {
       res.status(500).json(error);
     }
